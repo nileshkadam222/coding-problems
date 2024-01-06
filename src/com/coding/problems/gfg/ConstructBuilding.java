c45fa6f709b7d65c3fcebbf97a49bcb758820f77
Nilesh Ashok kadam <47922340+nileshkadam222@users.noreply.github.com>
2024-01-06 22:07:49 +0530

    GFG = Count possible ways to construct buildings

--- src/com/coding/problems/gfg/ConstructBuilding.java
+++ src/com/coding/problems/gfg/ConstructBuilding.java
@@ -4,28 +4,23 @@ public class ConstructBuilding {
 
 
 //User function Template for Java
-
-    class Solution
-    {
-        public int TotalWays(int N)
-        {
+        public int TotalWays(int N) {
             // Code here
             long mod = 1000000007;
-            if(N==1){
+            if (N == 1) {
                 return 4;
             }
             long first = 1;
             long second = 1;
-            long prev_f = 0,prev_s = 0;
-            for(int i=2;i<=N;i++){//normal fibonacci
+            long prev_f = 0, prev_s = 0;
+            for (int i = 2; i <= N; i++) {//normal fibonacci
                 prev_f = second;
-                second = (first + second)%mod ;
+                second = (first + second) % mod;
 
                 first = prev_f;
                 prev_s = second;
             }
-            long ans =  ((prev_f + prev_s));
-            return (int)((ans*ans)%mod);
+            long ans = ((prev_f + prev_s));
+            return (int) ((ans * ans) % mod);
         }
-    }
 }
