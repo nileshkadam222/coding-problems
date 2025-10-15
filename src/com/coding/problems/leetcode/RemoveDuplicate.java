2d6cf7bf4b994fcf8ef590f0449d323cdf6db3af
Nilesh Ashok kadam <47922340+nileshkadam222@users.noreply.github.com>
2025-10-15 06:29:42 +0530

    Remove duplicate using two pointer approach

---
+++ src/com/coding/problems/leetcode/RemoveDuplicate.java
@@ -0,0 +1,48 @@
+package com.coding.problems.leetcode;
+/*Remove Duplicates from Sorted Array : https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=problem-list-v2&envId=two-pointers
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+
+public class RemoveDuplicate {
+
+    public static int removeDuplicates(int[] nums) {
+        int pointer1 = 0;
+
+        for(int pointer2 = 1; pointer2 < nums.length; pointer2++){
+            if(nums[pointer2] != nums[pointer1]){
+                pointer1 ++;
+                nums[pointer1] = nums[pointer2];
+            }
+        }
+        return pointer1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int numOfElement = removeDuplicates(nums);
+        System.out.println("Number of elements: " + numOfElement);
+        System.out.print("elements : " + numOfElement);
+        for(int i=0; i< numOfElement; i++){
+            System.out.print(" " + nums[i]);
+        }
+
+
+    }
+}
