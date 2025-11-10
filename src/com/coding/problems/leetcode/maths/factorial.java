0ee9c05ab9ae5b3eb228d48038a97d14ab2467cd
Nilesh Ashok kadam <47922340+nileshkadam222@users.noreply.github.com>
2025-11-10 06:43:00 +0530

    Factorial

--- src/com/coding/problems/leetcode/maths/factorial.java
+++ src/com/coding/problems/leetcode/maths/factorial.java
@@ -1,15 +1,17 @@
 package com.coding.problems.leetcode.maths;
 
+import java.math.BigInteger;
+
 //Find the factorial of number.
 //5 => 5*4*3*2*1
 class factorial{
 
-	private static int getFactorial(int number){
-		if(number < 1){
-			return 1;
-		}
+	private static long getFactorial(long number){
+        if(number < 0){
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+        }
 
-		int factorial = 1;
+		long factorial = 1;
 		for(int i=1; i<= number; i++){
 			factorial = factorial * i;
 		}
@@ -19,10 +21,10 @@ class factorial{
 
     // 120 -> 1
     // 123	-> 0
-    private static int trallingZero(int number){
+    private static int trallingZero(long number){
         int count = 0;
         while(number != 0){
-            int lastDigit = number % 10;
+            long lastDigit = number % 10;
             if(lastDigit != 0){
                 break;
             }
@@ -33,9 +35,13 @@ class factorial{
     }
 
 	public static void main(String[] args){
-        int factOfFive = getFactorial(5);
-        int factOfThree = getFactorial(3);
+        long factOfFive = getFactorial(5);
+        long factOfThree = getFactorial(3);
+        long factOf13= getFactorial(13);
+        long factO30= getFactorial(30);
         System.out.println("5 -> " + factOfFive +  " Zero -> " + trallingZero(factOfFive));
         System.out.println("3 -> " + factOfThree +  " Zero -> " + trallingZero(factOfThree));
+        System.out.println("13 -> " + factOf13 +  " Zero -> " + trallingZero(factOf13));
+        System.out.println("30 -> " + factO30 +  " Zero -> " + trallingZero(factO30));
 	}
 }
